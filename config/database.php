f2119e56617eb5473cacae423d695dc227b761f6
Manish Kumar <manish966128@gmail.com>
2026-09-15 20:55:55 +0530

    feat: add Vercel deployment configuration and update MySQL SSL options

--- config/database.php
+++ config/database.php
@@ -61,7 +61,8 @@ return [
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 Mysql::ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
-            ]) : [],
+                Mysql::ATTR_SSL_VERIFY_SERVER_CERT => env('MYSQL_ATTR_SSL_VERIFY_SERVER_CERT'),
+            ], fn ($v) => ! is_null($v)) : [],
         ],
 
         'mariadb' => [
